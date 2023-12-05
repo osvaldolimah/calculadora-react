9804ca5d16b2cc9cdf0b7ae748ae213565682e49
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-12-04 21:35:01 -0300

    calculadora em react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Container, Content } from "./styles"
+import Input from './components/Input'
+import Button from './components/Button'
+
+function App() {
+ 
+  return (
+    <>
+    <Container> 
+      <Content>
+        <Input></Input>
+      </Content>
+      
+      </Container>
+      
+    </>
+  )
+}
+
+export default App
